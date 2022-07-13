2d1de9d7ecbe669f333019a5609b8d02bce5dad6
Vinicius Groto de oliveira <102178039+vinig79@users.noreply.github.com>
2022-07-13 00:17:02 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,54 @@
+from random import randint
+from velha import Velha
+velha = Velha()
+while True:
+    controle = []
+    c = 0
+    p2 = None
+    
+    print("=-"*10,"Jogo da velha","-="*10)
+    print("Escolha entre bolinha e xisinho:\nO\nX")
+    x_or_o = input(">>").upper()
+
+    if x_or_o != "X" and x_or_o != "O":
+        print("\n!!!! Apenas x e O !!!!\n")
+        continue
+    if x_or_o == "X":
+        p2 = "O"
+    else:
+        p2 = "X"
+
+    print("!! OK vamos joagar !!\n")
+
+    while c != 4:
+        print(velha.menu())
+        print("\nEscolha um numero corresponde a posição que deseja:")
+        a = int(input())
+        if not a in [x for x in range(1,10)] and a in controle:
+            print("apenas os números indicados e não selecionados")
+            continue
+        
+        velha.pos(a,x_or_o)
+        b = randint(1,9)
+        if b in controle:
+            continue
+        velha.pos(b,p2)
+        controle.append(a)
+        controle.append(b)
+        if velha.verify()[1]:
+            print(velha.verify()[0])
+            break
+        c += 1
+
+    print("=-"*10,"Vencedor","-="*10)
+    if not velha.verify()[1]:
+        print(velha.verify()[0])
+    print()
+
+    cont = input("deseja continuar??(s/n)").lower()
+    if cont != "n" and cont != "s":
+        print("Apenas S e N")
+        continue
+    elif cont == "n":
+        break
+    
---
+++ velha.py
@@ -0,0 +1,47 @@
+
+
+class Velha:
+    matriz = [["1","2","3"],["4","5","6"],["7","8","9"]]
+    def verify(self):
+
+        for x in range(0,3):
+            if self.matriz[x].count("X") == 3:
+                return (f" X Win's",True)
+            elif self.matriz[x].count("O") == 3:
+                return (f" O Win's",True)
+
+            elif self.matriz[0][x] == self.matriz[1][x] and self.matriz[1][x] == self.matriz[2][x]:
+                return (f"{self.matriz[0][x]} Win's",True)
+
+        if (self.matriz[0][0] == self.matriz[2][2] and self.matriz[0][0] == self.matriz[1][1]) or (self.matriz[0][2] ==   self.matriz[2][0] and 
+            self.matriz[0][2] == self.matriz[1][1]): 
+            return (f"{self.matriz[1][1]} Win's",True)
+            
+        return ("velha",False)
+    
+    def menu(self,):
+        return f"  {self.matriz[0][0]} | {self.matriz[0][1]} | {self.matriz[0][2]}\n----+---+----\n  {self.matriz[1][0]} | {self.matriz[1][1]} | {self.matriz[1][2]}\n----+---+----\n  {self.matriz[2][0]} | {self.matriz[2][1]} | {self.matriz[2][2]}"
+
+    def insert(self,pos1,pos2,player):
+        self.matriz[pos1][pos2] = player
+             
+    def pos(self,value,player):
+        match value:
+            case 1:
+                self.insert(0,0,player)
+            case 2:
+                self.insert(0,1,player)
+            case 3:
+                self.insert(0,2,player)
+            case 4:
+                self.insert(1,0,player)
+            case 5:
+                self.insert(1,1,player)
+            case 6:
+                self.insert(1,2,player)
+            case 7:
+                self.insert(2,0,player)
+            case 8:
+                self.insert(2,1,player)
+            case 9:
+                self.insert(2,2,player)
